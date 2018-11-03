22acee0db6821cd5834efea3ac03c72b1b2bd68e
Yufei Ren <yufei.ren@thomsonreuters.com>
2018-11-03 23:10:39 +0800

    update

--- sofie-client/src/main/java/me/lokvin/kiwi/sofe/App.java
+++ sofie-client/src/main/java/me/lokvin/kiwi/sofe/App.java
@@ -1,13 +1,21 @@
 package me.lokvin.kiwi.sofe;
 
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.core.util.StatusPrinter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public static void main( String[] args ) {
+        Logger log = LoggerFactory.getLogger(App.class);
+        log.debug("hello slf4j, {}, {}, {}", "one", "two", "three");
+
+        String[] strs = {"1", "2", "3"};
+        log.debug("hello slf4j {}, {}, {}", strs);
     }
 }
