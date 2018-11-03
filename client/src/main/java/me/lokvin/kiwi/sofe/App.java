729a11a0db941fa71fa72ef84c89c4cc86e9f9cd
Yufei Ren <yufei.ren@thomsonreuters.com>
2018-11-03 10:12:18 +0800

    update

---
+++ client/src/main/java/me/lokvin/kiwi/sofe/App.java
@@ -0,0 +1,13 @@
+package me.lokvin.kiwi.sofe;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
