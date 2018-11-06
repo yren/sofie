08b21c43af872d7ccd92bc8a70db0c10a2f00e75
Yufei Ren <yufei.ren@thomsonreuters.com>
2018-11-07 00:38:05 +0800

    update

---
+++ sofie-client/src/main/java/me/lokvin/kiwi/sofe/protocol/RequestEncapsulate.java
@@ -0,0 +1,34 @@
+package me.lokvin.kiwi.sofe.protocol;
+
+import me.lokvin.kiwi.sofe.exception.InvalidRequestException;
+import me.lokvin.kiwi.sofe.exception.SofieBaseException;
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.StringUtils;
+
+public class RequestEncapsulate extends ProtocolEncapsulateBase {
+
+    public static final String REQUEST_KEYWORD = "SOFIE_REQUEST";
+
+    private String requestCommand;
+    private String requestVersion;
+    private String[] commandParameters;
+    private char protocolSeparator;
+
+    private RequestEncapsulate(char protocolSeparator) {
+        this.protocolSeparator = protocolSeparator;
+    }
+
+    public static RequestEncapsulate parse(String requestBody) throws SofieBaseException {
+        char protocolSeparator = validateAndExtractSeparator(requestBody, REQUEST_KEYWORD, true);
+        String[] parts = StringUtils.split(requestBody, protocolSeparator);
+        if (parts == null || parts.length < 3) {
+            throw new InvalidRequestException("request not properly format.");
+        }
+
+        RequestEncapsulate request = new RequestEncapsulate(protocolSeparator);
+        request.requestVersion = parts[1];
+        request.requestCommand = parts[2];
+        request.commandParameters = ArrayUtils.subarray(parts, 3, parts.length);
+        return request;
+    }
+}
---
+++ sofie-client/src/main/java/me/lokvin/kiwi/sofe/util/SofieStringUtils.java
@@ -0,0 +1,15 @@
+package me.lokvin.kiwi.sofe.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SofieStringUtils {
+
+    public static final char DELIMITER_COMMA = ',';
+    public static final char DELIMITER_EQUAL = '=';
+    public static final char DELIMITER_DOT = '.';
+    public static final String SPECIAL_NULL_VALUE = "<NULL>";
+
+    private static final Logger log = LoggerFactory.getLogger(SofieStringUtils.class);
+
+}
